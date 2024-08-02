43bcef23cbc5b70cc1cc20bf6a627b607f822a11
Muhamad Rifqi Hilmy Apriadi <muhamadrifqi240@gmail.com>
2024-08-03 01:16:02 +0700

    [FIX] add meta security

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8" />
     <meta name="viewport"
         content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>@yield('title')</title>
 
     <meta name="description" content="" />
